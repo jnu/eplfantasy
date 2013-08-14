3915e440232532edf670003ae015b5399647424c
Joseph Nudell <joenudell@gmail.com>
2013-08-14 17:11:53 -0500

    fixed player uniqueness constraints

--- optimize_roster.py
+++ optimize_roster.py
@@ -30,6 +30,8 @@ from pprint import pprint
 from sys import stderr, exit
 import eplstats
 import argparse
+import os
+import re
 
 
 def get_player_stats(season=2014, benchfrac=.1):
@@ -38,13 +40,18 @@ def get_player_stats(season=2014, benchfrac=.1):
     players = []
 
     _positions = ['forwards', 'midfielders', 'defenders', 'keepers']
+    _id = 0
+    _uid = 990000
 
     for position in _positions:
         _players = eplstats.get(position, season=season)
 
         for player in _players:
+            _id += 1
+
             for captain in [0, 1]:
                 for pfx in ['', 'sub-']:
+                    _uid += 1
 
                     # Create a postfix for the name including semantic details
                     # about the given options
@@ -63,12 +70,20 @@ def get_player_stats(season=2014, benchfrac=.1):
                     stats = {
                         'cost' : player.cost,
                         'score' : points,
-                        'name' : ("%s %s - %s - %s" % \
+                        'pid' : _id,
+                        'uid' : _uid,
+                        'bench' : "starter" if not len(pfx) else "sub",
+                        'position' : position[:-1],
+                        'fname' : player.first_name,
+                        'lname' : player.last_name,
+                        'club' : player.club,
+                        'name' : ("%s %s - %s - %s (%d)" % \
                             (
                                 player.first_name,
                                 player.last_name,
                                 position[:-1],
-                                postfix
+                                postfix,
+                                _uid
                             )).strip(),
                         'captain' : captain,
                         'keeper' : 0,
@@ -86,6 +101,12 @@ def get_player_stats(season=2014, benchfrac=.1):
 
                     players.append(stats)
 
+    all_ids = range(_id + 1)
+    for player in players:
+        for i in all_ids:
+            player['id%d' % i] = float(player['pid']==i)
+
+
     return players
 
 
@@ -93,9 +114,9 @@ def get_player_stats(season=2014, benchfrac=.1):
 if __name__=='__main__':
     # Defaults
     season = 2014
-    tolerance = .05
+    tolerance = 10**-6
     budget = 100.
-    bench = 1/26.
+    bench = 1/10.
 
     # Get CL params
     parser = argparse.ArgumentParser(description=__doc__)
@@ -103,11 +124,11 @@ if __name__=='__main__':
     parser.add_argument('-s', '--season', type=int, default=season,
         help="ESPN endpoint only currently supports 2014 season")
     parser.add_argument('-t', '--tolerance', type=float, default=tolerance,
-        help="Fuzzy boundaries around optimal solution, default is .05")
+        help="Fuzzy boundaries around optimal solution, default is 10e-6")
     parser.add_argument('-b', '--budget', type=float, default=budget,
         help="Salary cap in millions of pounds, default is 100")
     parser.add_argument('-e', '--bench', type=float, default=bench,
-        help="Fraction of score to reduce substitutes by, default is .1 (=1/10 of starter score")
+        help="Fraction of score to reduce substitutes by, default is 1/10")
 
     cli = parser.parse_args()
 
@@ -122,7 +143,11 @@ if __name__=='__main__':
     players = get_player_stats(season=season, benchfrac=bench)
     print >>stderr, "done."
 
+
     # Define constraints
+    print >>stderr, "Defining constraints ...",
+    all_ids = range(players[-1]['pid']+1)
+    
     constraints = lambda values : (
             values['cost'] <= budget,
             ##
@@ -145,21 +170,74 @@ if __name__=='__main__':
             ##
             values['sub-forward'] + values['sub-midfielder'] \
                 + values['sub-defender'] + values['sub-keeper'] == 4,
-            values['nItems'] == 15 # redundant now
-        )
+            ##
+            #values['nItems'] == 15 # redundant now
+        )+tuple([values['id%d'%i]<=1 for i in all_ids])
+
+    print >>stderr,"done."
+
 
     # Define objective: maximize score
     objective = [
         'score', tolerance, 'max'
     ]
 
+
+    print >>stderr, "Solving problem ..."
+
     # Construct problem
     p = KSP(objective, players, constraints=constraints, name='ksp_mop')
 
     # Run optimizer
-    r = p.solve('glpk', plot=1, iprint=1, nProc=2)
+    r = p.solve('glpk', iprint=1, nProc=2)
 
 
     # Output solution
     print >>stderr, "Best solution found:"
     pprint(r.xf)
+
+
+    os.system('clear')
+    names = r.xf
+
+    # Find selected players in `players` list
+    uids = []
+    for name in names:
+        # Get unique id from name
+        m = re.search(r'\((\d+)\)', name)
+        if m is None:
+            raise ValueError("Can't find UID in %s" % name)
+
+        uid = int(m.group(1))
+
+        uids.append(uid)
+
+    roster = []
+    for player in players:
+        if player['uid'] in uids:
+            roster.append(player)
+
+    # Print details about selected roster
+    row_format= "{:<15}{:<15}{:<15}{:<14}{:<6}{:<5}{:<7}"
+
+    print row_format.format('First Name',
+        'Last Name', 'Position', 'Starting', 'Capt.', 'Club', 'Salary')
+
+    total_cost = 0.
+    for player in roster:
+        total_cost += player['cost']
+        print row_format.format(player['fname'],
+            player['lname'], player['position'], player['bench'],
+            "X" if player['captain'] else "",
+            player['club'], player['cost'])
+
+    print 
+    print "Total Cost:\t", total_cost, "M"
+    print "Under budget:\t", budget-total_cost, "M"
+    print
+
+
+
+
+
+
